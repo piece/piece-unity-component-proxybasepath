d10632c31329f1b10043c0bf4cd1d6cc6ef1c144
KUBO Atsuhiro <kubo@iteman.jp>
2009-03-04 23:02:10 +0900

    - Replaced SVN: string with GIT:.

--- tests/prepare.php
+++ tests/prepare.php
@@ -32,7 +32,7 @@
  * @subpackage Piece_Unity_Component_ProxyBasePath
  * @copyright  2006-2009 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
- * @version    SVN: $Id$
+ * @version    GIT: $Id$
  * @since      File available since Release 1.0.0
  */
 
